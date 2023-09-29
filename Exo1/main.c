cd7ac89c2a99a8d4111c1b1a5d67a2b134c603e7
lalin <lalin@CESTAMOI>
2023-09-29 12:10:37 +0200

    Ajoutez des fichiers projet.

---
+++ Exo1/main.c
@@ -0,0 +1,94 @@
+﻿// This directive deactivate Warnings for unsecure functions that are deprecated by Microsoft.
+// This is not a good practice but is used for pedagogic purposes,
+// allowing usage of standard C functions as described in manuals.
+// You should use only secured functions named with _s postfix
+// e.g. printf_s() rather than printf()
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+
+/******************************************/
+/*   TPC2022 no 1  exo1                   */
+/*                                        */
+/******************************************/
+
+// le type 'char' caractérise un entier positif codé sur 8 bits compris entre -128 et +127,
+// le bit de poids fort sert au codage du signe.
+// un 'unsigned char' pourra donc représenter un nombre positif compris en 0 et 255, 
+// soit 256 valeurs.
+// La représentation en hexadécimale de 255 est FF (notée 0xFF)
+// Le type booléen n'existe pas en C90, une valeur non nulle 
+// dans une expression logique est comprise comme 'VRAI'.
+// La prise en charge du type booléen peut se faire en ajoutant une directive #include <stdbool.h>
+
+// Compléter le code par des appels à la fonction d’affichage printf pour afficher la valeur 
+// de chacune des variables dans un format correct. Attention, le choix du filtre %, modifie le résultat
+// car il réalise une conversion de la donnée avant son affichage dans le format souhaité.
+// Donc, ce qui s’affiche est ce qui a été demandé, et pas nécessairement la donnée dans son format d’origine.
+// Donner pour chaque type, le nombre d’octets utilisés pour le codage, et indiquer quelle valeur minimum
+// et quelle valeur maximum(dynamique) on peut représenter avec une variable qui aurait ce type.
+// Donner la réponse sous la forme de commentaires dans votre fichier source.
+
+// définition de type
+typedef unsigned char byte;
+
+int main() { // accolade ouvrante = début de bloc d'instructions
+	// Déclaration de variables réservées au main(), et initialisations facultatives
+	char OneLetter = 'A';
+	printf("exemple d'affichage d'un caractere %c, codage sur %d octets\n", OneLetter, sizeof(OneLetter));
+	//"char" valeur compris entre -128 et +127
+	char AnOtherLetter = 66;
+	printf("\nexemple d'affichage d'un entier court %c, codage sur % d octets\n", AnOtherLetter, sizeof(AnOtherLetter));
+	//"char" valeur compris entre -128 et +127
+	byte toto = 0;
+	printf("\nexemple d'affichage d'un entier court %d, codage sur % d octets\n", toto, sizeof(toto));
+	//"byte"= 0 ou 1
+	unsigned char VerySmallCounter = 255;
+	printf("\nexemple d'affichage d'un entier court %c, codage sur % d octets\n", VerySmallCounter, sizeof(VerySmallCounter));
+	//"unsigned char" valeur compris en 0 et 255
+	short SmallCounter = 32767;
+	printf("\nexemple d'affichage d'un entier court %hd, codage sur %d octets\n", SmallCounter, sizeof(SmallCounter));
+	//"short" valeur comprise entre -32 768 et 32 767
+	int Counter = 0;
+	printf("exemple d'affichage d'un entier court %d, codage sur %d octets\n", Counter, sizeof(Counter));
+	//"int" valeur comprise entre -2 147 483 648 et 2 147 483 648 
+	long LargeCounter = 2147483647;
+	printf("\nexemple d'affichage d'un entier court %ld, codage sur % d octets\n", LargeCounter, sizeof(LargeCounter));
+	//"long" valeur comprise entre −2 147 483 647 et 2 147 483 647
+	unsigned long FullRangeLargeCounter = 4294967295;
+	printf("\nexemple d'affichage d'un entier court %lu, codage sur % d octets\n", FullRangeLargeCounter, sizeof(FullRangeLargeCounter));
+	//"unsigned long" valeur comprise entre 0 et 4 294 967 295
+	int ChienDeGarde = 0;
+	printf("\nexemple d'affichage d'un entier court %d, codage sur % d octets\n", ChienDeGarde, sizeof(ChienDeGarde));
+	// "int" valeur comprise entre -2 147 483 648 et 2 147 483 648
+	float Dim1 = 1.414F;
+	printf("\nexemple d'affichage d'un entier court %f, codage sur % d octets\n", Dim1, sizeof(Dim1));
+	//"float" valeur comprise entre 
+	double Dim2 = .5;
+	printf("\nexemple d'affichage d'un entier court %f, codage sur % d octets\n", Dim2, sizeof(Dim2));
+	//"double" valeur comprise entre 
+
+	bool TestResult = 1;
+	printf("\nexemple d'affichage d'un entier court %d, codage sur % d octets\n", TestResult, sizeof(TestResult));
+	// Déclaration de constante
+	const double Pi = 3.14159265;
+	printf("\n%f", Pi, sizeof(Pi));
+	
+	int A = 1, B = 2, C = 3;
+	printf("\n %d (%p)\n %d p)\n %d (%p)\n", A,&A,B,&B,C,&C);
+
+
+	// les instructions du programme principal : main()
+	/********** Debut de programme *****************/
+	
+	/*********** Fin de programme ******************/
+
+	return(EXIT_SUCCESS);
+} // accolade fermante = fin de bloc d'instructions
+
+
---
+++ Exo2/main.c
@@ -0,0 +1,80 @@
+﻿// This directive deactivate Warnings for unsecure functions that are deprecated by Microsoft.
+// This is not a good practice but is used for pedagogic purposes,
+// allowing usage of standard C functions as described in manuals.
+// You should use only secured functions named with _s postfix
+// e.g. printf_s() rather than printf()
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <float.h>
+
+/******************************************/
+/*   TPC2022 no 1  exo 2                   */
+/*                                        */
+/******************************************/
+
+
+
+int main() {
+
+	// a-
+	// 
+	// Donnez le code qui permet de calculer la valeur de Pi  en utilisant la relation ArcTang(1) = Pi/4 et le développement limité
+	// de la fonction Arctangente :
+	//
+	// Pi = 4 * ( 1 - 1/3 + 1/5 - 1/7 + 1/9 -1/11 + ... ) 
+	// 
+	// On arrêtera de calculer les termes de l'addition quand leur valeur sera inférieure à 10e-10
+	// Afficher le résultat avec 10 décimales.
+	// 
+	double Pi = 0.0;
+	int n = 0;
+	double termes;
+	for (n = 0; ; n++)
+	{
+		termes = (4.0 * (float)(1.0 / (float)(2 * n + 1)) * (float)pow((-1), n));
+		Pi = Pi + termes;
+
+		if (4.0 * (float)(1.0 / (float)(2 * n + 1)) < 10e-10) { break; };
+
+	}
+	printf("La valeur de Pi est %f\n", Pi);
+
+
+	// Ou le code si dessous aussi 
+	// 
+	//while (n<400) {
+	//	Pi = Pi + (4.0 * (float)(1.0 / (float)(2 * n + 1)) * (float)pow((-1), n));
+	//	n = n + 1;
+	//}
+	//printf("%f\n", Pi);
+
+	
+
+
+	// b- 
+	// 
+	// Affichez la table des sinus des angles allant de 0 à 90 degrés en faisant varier l'angle de 10 degrés en 10 degrés.
+	// Quel fichier d'entêtes .h doit-on utiliser ? On doit utiliser maths.h
+	// Définissez la valeur de PI à l'aide d'une constante en lui donnant la valeur 3.1415926535898
+	// Afficher la valeur de l'angle en degrés et la valeur du sinus sur la même ligne en alignant les valeurs
+	//
+
+	int angle = 0;
+	double PI = 3.1415926535898;
+	while (angle <= 90) {
+		printf("La valeur du sinus de l'angle %d est de %f\n", angle, sin(angle * (PI / 180)));
+		angle = angle + 10;
+	}
+
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
